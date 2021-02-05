8a7e4020abbf6cc7199ef8e5440b7c6350c61a80
Jared Searle <jjsearle@usf.edu>
2021-02-05 11:15:58 -0500

    Add project files.

---
+++ JaredSearleISM4300Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace JaredSearleISM4300Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
+            foreach (int val in numbers)
+            {
+                Console.WriteLine("Element value =" + val.ToString());
+            }
+            Console.ReadKey(true);
+
+        }
+
+
+
+
+
+
+        
+    }
+}
+
